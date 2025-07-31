e926b5617404fadd852cbe45a8e9680f80116e4d
kafella92 <choidj10@gmail.com>
2025-07-31 14:43:09 +0900

    기본세팅 및 loginUno 인트 처리

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -8,9 +8,9 @@ public class UserController {
         return instance;
     }
 
-    // (*) 로그인 넘버
+    // (*) 로그인 넘버 loginUno
     // 0이면 로그인 안 된 상태, 1이면 관리자 로그인, 2부터는 일반회원 로그인 (uno와 같음)
     // Note, Product에서는 2보다 큰 숫자여야하고, Sample에서는 숫자 1 상태여야 합니다.
-    public int loginUno = 0;
+    public static int loginUno = 0;
 
 }
