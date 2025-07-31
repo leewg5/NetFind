ebb4f75f947644f98ac8032b4851e672b712f594
kant300 <p.o.i.u.0987552@gmail.com>
2025-07-31 18:00:47 +0900

    Dto, Controller(1/2/3) , Dao( DB연동)

--- src/model/dao/SampleDao.java
+++ src/model/dao/SampleDao.java
@@ -1,11 +1,40 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class SampleDao {
     // (*)싱글톤
-    private SampleDao(){}
+    private SampleDao(){ connect(); }
     private static final SampleDao instance = new SampleDao();
     public static SampleDao getInstance(){
         return instance;
     }
+
+    // (*) DB 연동
+    private String db_url = "jdbc:mysql://localhost:3306/netFind";
+    private String db_user = "root";
+    private String db_password = "1234";
+    private Connection conn;
+
+    private void connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(db_url, db_user, db_password);
+            // System.out.println( "연동성공");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+
+    // (1) 샘플 등록기능구현
+
+    // (2) 샘플 삭제기능 구현
+
+    // (3) 샘플 조회기능 구현
+
+    // (4) 샘플번호와 제품번호 비교
+
 
 }// class e
--- src/model/dto/SampleDto.java
+++ src/model/dto/SampleDto.java
@@ -1,4 +1,72 @@
 package model.dto;
 
 public class SampleDto {
+    // 1. 멤버변수
+    private int sno;
+    private String sname;
+    private String sspec;
+    private String smaker;
+    private String sunit;
+
+    public SampleDto() {
+    }
+
+    public SampleDto(int sno, String sname, String sspec, String smaker, String sunit) {
+        this.sno = sno;
+        this.sname = sname;
+        this.sspec = sspec;
+        this.smaker = smaker;
+        this.sunit = sunit;
+    }
+
+    public int getSno() {
+        return sno;
+    }
+
+    public void setSno(int sno) {
+        this.sno = sno;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getSspec() {
+        return sspec;
+    }
+
+    public void setSspec(String sspec) {
+        this.sspec = sspec;
+    }
+
+    public String getSmaker() {
+        return smaker;
+    }
+
+    public void setSmaker(String smaker) {
+        this.smaker = smaker;
+    }
+
+    public String getSunit() {
+        return sunit;
+    }
+
+    public void setSunit(String sunit) {
+        this.sunit = sunit;
+    }
+
+    @Override
+    public String toString() {
+        return "SampleDto{" +
+                "sno=" + sno +
+                ", sname='" + sname + '\'' +
+                ", sspec='" + sspec + '\'' +
+                ", smaker='" + smaker + '\'' +
+                ", sunit='" + sunit + '\'' +
+                '}';
+    }
 }
